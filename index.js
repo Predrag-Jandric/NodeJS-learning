988d3ec84f98150dd4986fdac1ad277bb17aa4e8
predrag jandric <predrag.jandric.bg@gmail.com>
2024-12-22 01:14:53 +0100

    will ADD: fill out templates, play around with NPM packages and add routing for all individual elements from the map

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const slugify = require("slugify");
+
+const replaceTemplate = require("./modules/replaceTemplate");
+
 // /////////////////////////////
 // FOR FILES
 
@@ -37,19 +41,53 @@ const url = require("url");
 
 // /////////////////////////////
 // FOR SERVER
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
-  const pathName = req.url;
+const slugs = dataObj.map((el)=> slugify(el.productName, { lower: true }))
 
-  if (pathName === "/overview" || pathName === "/") {
-    res.end("overview page");
-  } else if (pathName === "/product") {
-    res.end("product page");
-  } else if (pathName === "/api") {
+console.log(slugs);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // overview page
+  if (pathname === "/overview" || pathname === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join();
+    // console.log(cardsHtml);
+    // console.log(dataObj);
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // product page
+  } else if (pathname === "/product") {
+    const product = dataObj[query.id];
+    res.writeHead(200, { "content-type": "text/html" });
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // api
+  } else if (pathname === "/api") {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(data);
+
+    // not found page
   } else {
     res.writeHead(404, {
       "content-type": "text/html",
