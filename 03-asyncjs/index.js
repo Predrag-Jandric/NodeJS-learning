64899c54fb243eec22707e130f78b0859ce1aaab
predrag jandric <predrag.jandric.bg@gmail.com>
2024-12-27 00:14:39 +0100

    will ADD: amother wrong way of async fetching, this time building a promise so that .then() can be chained on it, but there is even better way of fetching with async/await

--- 03-asyncjs/index.js
+++ 03-asyncjs/index.js
@@ -1,17 +1,36 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`breed ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      console.log(res.body.message);
-      if (err) return console.log(err.message);
-
-      fs.writeFile("dog-img.txt", res.body.message, (err) => {
-        console.log("dog img saved");
-      });
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) return reject("there is an error");
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile((file, data) => {
+      if (err) reject("error bla bla");
+      resolve("success");
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`breed ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    return writeFilePro("dog-img.txt", res.body.message);
+  })
+  .then(() => {
+    console.log("im am string");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
