a0911e6513fe415f480913f8f62c5578941a2c2c
predrag jandric <predrag.jandric.bg@gmail.com>
2024-12-27 01:08:05 +0100

    will ADD: best way of fetching data using async/await

--- 03-asyncjs/index.js
+++ 03-asyncjs/index.js
@@ -11,26 +11,30 @@ const readFilePro = (file) => {
 };
 
 const writeFilePro = (file, data) => {
+  // Single writeFilePro function
   return new Promise((resolve, reject) => {
-    fs.writeFile((file, data) => {
-      if (err) reject("error bla bla");
-      resolve("success");
+    fs.writeFile(file, data, (err) => {
+      // Correct callback implementation
+      if (err) reject("Error while writing to the file");
+      resolve("File written successfully");
     });
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`breed ${data}`);
 
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    return writeFilePro("dog-img.txt", res.body.message);
-  })
-  .then(() => {
-    console.log("im am string");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+    await writeFilePro("dog-img.txt", res.body.message);
+    console.log("random dog image saved to file");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDogPic();
